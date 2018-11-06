984413dba56e8484a4c2615f837b05735c5eae85
Nikola Selic <selich.work@gmail.com>
2018-11-06 20:01:07 +0100

    Added .gitignore

--- main.cpp
+++ main.cpp
@@ -1,23 +1,62 @@
 #include <iostream>
+#include <vector>
 
 // input layer -> hidden layer 
+
+using namespace std;
+
+class Neuron {};
+
+
+typedef vector<Neuron> Layer;
+
 class Net {
 
 private:
+    vector<Layer> m_layers; // layerNumber , neuronNumber
 
 public:
+    Net(const vector<unsigned> &topo);
+    void feedForward(const vector<double> &inputVal) {};
+    void backProp(const vector<double> &targetVal) {};
+    void getResults(const vector<double> &resultVal) const {};
 
 
 
 
 };
+Net::Net(const vector<unsigned> &topo){
+    for (unsigned layerNumber = 0; layerNumber < topo.size(); ++layerNumber)
+    {
+        // append new Layer
+        m_layers.push_back(Layer());
+
+        
+        // needed for one more biased neuron ( <= )
+        for( unsigned neuronNumber = 0; neuronNumber <= topo[layerNumber]; ++neuronNumber)
+        {
+            m_layers.back().push_back(Neuron());
+            cout << "New neuron created" << endl;
+        }
+        
+        
+    }
+}
 
 int main(){
 
+    vector<unsigned> topo;
+    topo.push_back(3);
+    topo.push_back(3);
+    topo.push_back(1);
     Net net(topo);
 
-    net.feedForward(input);
+    vector<double> inputVal;
+    vector<double> targetVal;
+    vector<double> resultVal;
+    net.feedForward(inputVal);
     // testing outputs
-    net.
+    net.backProp(targetVal);
+    net.getResults(resultVal);
 
 }
